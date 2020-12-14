59a11c92da2b26cbd8ce83ea6c16e780d3a60022
Arseny Mitin <mitinarseny@gmail.com>
2020-12-14 23:13:25 +0300

    feat: nord colors

--- config.h
+++ config.h
@@ -7,15 +7,28 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char NORD[][16]        = {
+	"#2e3440",
+	"#3b4252",
+	"#434c5e",
+	"#4c566a",
+	"#d8dee9",
+	"#e5e9f0",
+	"#eceff4",
+	"#8fbcbb",
+	"#88c0d0",
+	"#81a1c1",
+	"#5e81ac",
+	"#bf616a",
+	"#d08770",
+	"#ebcb8b",
+	"#a3be8c",
+	"#b48ead",
+};
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg        bg        border   */
+	[SchemeNorm] = { NORD[6],  NORD[0],  NORD[1]  },
+	[SchemeSel]  = { NORD[6],  NORD[3],  NORD[3]  },
 };
 
 /* tagging */
@@ -56,7 +69,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run",
+	"-m", dmenumon,
+	"-fn", dmenufont,
+	"-nb", NORD[0],
+	"-nf", NORD[6],
+	"-sb", NORD[8],
+	"-sf", NORD[0],
+	NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
